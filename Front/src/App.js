88a92532d1e6e785ba58107db7c0c4dd428198d9
Miaherbillon <herbillon.mia@gmail.com>
2023-10-25 10:22:21 +0200

    Header

--- Front/src/App.js
+++ Front/src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Accueil from "./Pages/Accueil"; // Votre composant Accueil
 
+import Header from "./components/Header";
+
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Accueil />} />
       </Routes>
